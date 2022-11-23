982c793ab75b3c9082f74bd831f6a2ad7688dde4
Giovanny Montinny de Almeida Dantas <gmontinny@hotmail.com>
2022-11-23 05:08:14 -0400

    Update BookServiceImpl.java

--- src/main/java/com/onetomany/relacionamento/service/impl/BookServiceImpl.java
+++ src/main/java/com/onetomany/relacionamento/service/impl/BookServiceImpl.java
@@ -18,20 +18,20 @@ public class BookServiceImpl implements BookService {
 
     public Book saveBook(Book book) {
         List<Story> storyList = new ArrayList<>();
-        // create first story
+        // criando primeira story
         Story story1 = new Story();
         story1.setStoryName("Arrays");
-        // create second story
+        // criando segundo second story
         Story story2 = new Story();
         story2.setStoryName("Pointers");
-        // create third story
+        // criando terceiro story
         Story story3 = new Story();
         story3.setStoryName("Loops");
-        // add all story into storyList. Till here we have prepared data for OneToMany
+        // adicionado store na storyList. Até aqui, preparamos dados para OneToMany
         storyList.add(story1);
         storyList.add(story2);
         storyList.add(story3);
-        // Prepare data for ManyToOne
+        // Preparar dados para ManyToOne
         story1.setBook(book);
         story2.setBook(book);
         story3.setBook(book);
